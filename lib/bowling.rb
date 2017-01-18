ba2e1a03061e1e1ecd4e2b401fe9cd87b4346ce9
shoya sakaguchi <shouya_sakaguchi@pure-system.com>
2017-01-18 03:44:18 +0000

    add_scoreメソッドの引数名の修正

--- lib/bowling.rb
+++ lib/bowling.rb
@@ -3,8 +3,8 @@ class Bowling
         @add_score = 0
         @total_score = 0
     end
-    def add_score(score)
-        @add_score = score
+    def add_score(pins)
+        @add_score = pins
         @total_score += @add_score
     end
     def total_score
